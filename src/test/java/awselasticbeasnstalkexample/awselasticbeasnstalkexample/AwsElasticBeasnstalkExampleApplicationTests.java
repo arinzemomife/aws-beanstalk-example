932f444399b8929106159969509309101fdb78e8
arinzemomife <35673925+arinzemomife@users.noreply.github.com>
2019-10-07 00:13:14 -0500

    Aws Beanstalk example -initial commit

---
+++ src/test/java/awselasticbeasnstalkexample/awselasticbeasnstalkexample/AwsElasticBeasnstalkExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package awselasticbeasnstalkexample.awselasticbeasnstalkexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsElasticBeasnstalkExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
